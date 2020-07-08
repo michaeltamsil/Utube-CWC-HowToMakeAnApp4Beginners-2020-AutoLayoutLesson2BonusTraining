02fdc4ff3f4c9198f86a77c4f6bc55389539bbb3
Michael Tamsil <michaeltamsil@gmail.com>
2020-07-08 09:59:56 +0700

    initial commit

---
+++ AutoLayout (Leson2)BonusTraining/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayout (Leson2)BonusTraining
+//
+//  Created by michael tamsil on 07/07/20.
+//  Copyright © 2020 michael tamsil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
